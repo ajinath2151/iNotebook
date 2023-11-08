5ea0d5f33dd67d195cdf99e0fe3828ba0b619b0b
Ajinath <ajinath2151@gmail.com>
2023-11-08 23:24:16 +0530

    working on add data on database

--- backend/index.js
+++ backend/index.js
@@ -32,9 +32,12 @@ connectToMongo();
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+// middle ware for use request.body
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
 app.use('/api/auth', require('./routes/auth')) //auth.js
 app.use('/api/notes', require('./routes/notes')) //auth.js
 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const User = require('../models/User');
 const router = express.Router();
 
-router.get('/', (req, res)=>{
-    let obj = {
-        Name : 'Ajinath Shinde',
-        salary:34000,
-    }
-    res.json(obj);
+
+// create a use ruing : POST "/api/auth"  , dosen't require for auth
+router.post('/', (request, response)=>{    
+    console.log(request.body);    
+    // response.send("hello")
+    // create new users
+    const user = User(request.body)
+    user.save();
+    response.send(request.body)    
 })
 
 module.exports = router
